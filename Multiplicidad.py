d52287f12125dc4d7a5aed7b960acfb0c2490967
pablo.agudo <pablito_2_@hotmail.com>
2018-07-22 01:53:22 +0200

    Multiplicidad

---
+++ Multiplicidad.py
@@ -0,0 +1,11 @@
+
+
+
+numero_usuario = int(input("Numero que quieres la tablad e multiplicar:"))
+
+tabla = range(5,16)
+
+for multiplo in tabla:
+
+    if type(numero_usuario*multiplo/2) == :
+        print("{} * {} = {}".format(multiplo, numero_usuario, numero_usuario*multiplo) )
