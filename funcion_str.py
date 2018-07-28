bb2a269ac3598b14a78a5397589f66d1137fe10f
pablo.agudo <pablito_2_@hotmail.com>
2018-07-28 23:33:04 +0200

    funciones_str

--- funcion_str.py
+++ funcion_str.py
@@ -1,17 +1,20 @@
 
 def reverse_string(str_to_reverse):
-    str_to_reverse = input("Dime una str: ")
     posicion_letra = -1
     str_mod = ""
     for letra in str_to_reverse:
         str_mod += str_to_reverse[posicion_letra]
-        posicion_letra += 1
+        posicion_letra -= 1
 
     return str_mod
 
 
-reverse_string()
-print(str_to_reverse)
+print(reverse_string("Hola"))
+print(reverse_string("estenocleidomastoideo"))
+print(reverse_string("olaf"))
+print(reverse_string("paco el paquito me comio el pepenito"))
+
+
 
 
 
