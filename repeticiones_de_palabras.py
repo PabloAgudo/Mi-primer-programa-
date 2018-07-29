533385948182fb0ca8cb2944b51305c3d85887e5
pablo.agudo <pablito_2_@hotmail.com>
2018-07-30 00:41:52 +0200

    Repeticion de palabras en un texto

--- repeticiones_de_palabras.py
+++ repeticiones_de_palabras.py
@@ -1,13 +1,30 @@
-
-aparicion_palabras = dict ()
+signos = [".", ",", "!", "¡", "?", "¿", ":", ";"]
+aparicion_palabras = dict()
 
 texto = input("Introduzca un texto: ")
+indice = 0
+texto += " "
+palabras_texto = []
+caracteres = ["", ]
 
 
-for palabra in texto:
+for letra in texto:
+    if letra == " ":
+        palabras_texto.append(caracteres[0])
+        caracteres = [""]
+    elif letra in signos:
+        palabras_texto.append(caracteres[0])
+        caracteres = [""]
+    else:
+        caracteres[0] += letra
+
+
+for palabra in palabras_texto:
     if palabra in aparicion_palabras:
         aparicion_palabras[palabra] +=1
     else:
         aparicion_palabras[palabra] = 1
 
-print(aparicion_palabras)
+for cantidad_de_palabras in aparicion_palabras:
+    print("{}: {}".format(cantidad_de_palabras, aparicion_palabras[cantidad_de_palabras]))
+    print("")
